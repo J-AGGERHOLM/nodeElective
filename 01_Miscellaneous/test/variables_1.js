2d4e4897bb46ef470aa99bb769f8f94278db4137
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-01-29 10:33:03 +0100

    initial commit

---
+++ 01_Miscellaneous/test/variables_1.js
@@ -0,0 +1,18 @@
+//Node REPL
+
+//type coercion
+//Strict equality
+
+
+//Rule 1. Use const per default unless it needs to change, then use let
+const luckyNumber = 4;
+
+console.log(luckyNumber);
+
+//JS Data types
+//String, Number, Boolean, Object, BigInt, Symbol, null, undefined
+
+const person = {
+    //key-value pair
+    name: "Mathias"
+};
