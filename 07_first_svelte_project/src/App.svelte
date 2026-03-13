5a52ba5db1195a6c3f06b00fa030b52b40ef0df9
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-13 10:49:03 +0100

    svelte counters created

---
+++ 07_first_svelte_project/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Counter from "./components/counter/Counter.svelte";
+
+</script>
+<h1> Welcome to my first Scelte Project </h1>
+
+<h2> what do you want to see?</h2>
+<Counter countdownFrom={10}></Counter>
+<Counter countdownFrom={100}></Counter>
---
+++ 07_first_svelte_project/src/components/counter/Counter.svelte
@@ -0,0 +1,12 @@
+<script>
+    const { countdownFrom } = $props()
+    let count = $state(countdownFrom);
+
+    function descreaseCount() {
+        count -=1;
+    }
+</script>
+
+<h1>Counter</h1>
+<h4>{count}</h4>
+<button onclick={descreaseCount}> decrease the count</button>
