af08dcbcb9372b58240015d061b25ea0c2787682
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-15 10:47:23 +0100

    navigation active link re-implemented after refactoring

--- 100_Mandatory_01/util/templatingEngine.js
+++ 100_Mandatory_01/util/templatingEngine.js
@@ -17,7 +17,17 @@ export function constructPage(page, options = {}) {
 
   return (
     header.replaceAll(`$$DOCUMENT_TITLE$$`, options.documentTitle || "Node.js Documentation") +
-    navigation +
+    navigation
+      .replace(`$$OVERVIEW_ACTIVE$$`, options.active === "overview" ? "text-primary" : "")
+      .replace(`$$ABOUT_ACTIVE$$`, options.active === "about" ? "text-primary" : "")
+      .replace("$$ASSIGNMENTS_ACTIVE$$", options.active === "assignments" ? "text-primary" : "")
+      .replace("$$NOTE_01_ACTIVE$$", options.active === "note-01" ? "text-primary" : "")
+      .replace("$$NOTE_02_ACTIVE$$", options.active === "note-02" ? "text-primary" : "")
+      .replace("$$NOTE_03_ACTIVE$$", options.active === "note-03" ? "text-primary" : "")
+      .replace("$$NOTE_04_ACTIVE$$", options.active === "note-04" ? "text-primary" : "")
+      .replace("$$NOTE_05_ACTIVE$$", options.active === "note-05" ? "text-primary" : "")
+      .replace("$$NOTE_06_ACTIVE$$", options.active === "note-06" ? "text-primary" : "")
+      .replace("$$NOTE_07_ACTIVE$$", options.active === "note-07" ? "text-primary" : "") +
     page +
     markDownSection +
     footer
