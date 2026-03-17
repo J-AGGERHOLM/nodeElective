ae1a49bd80bc3f4cb7245bac5bd3f29e36a631d5
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-17 11:36:40 +0100

    tried with fileURLToPath in readPage

--- 100_Mandatory_01/util/templatingEngine.js
+++ 100_Mandatory_01/util/templatingEngine.js
@@ -1,8 +1,12 @@
 import fs from "fs";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export function readPage(filePath) {
-  return fs.readFileSync(path.join(process.cwd(), filePath)).toString();
+  return fs.readFileSync(path.resolve(__dirname, "..", filePath)).toString();
 }
 
 export function constructPage(page, options = {}) {
