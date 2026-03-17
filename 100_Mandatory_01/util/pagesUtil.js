7f59cb31c805cafc1f5c77217716c1fba0c6e3f5
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-17 10:14:19 +0100

    pathing for deployment debugging

--- 100_Mandatory_01/util/pagesUtil.js
+++ 100_Mandatory_01/util/pagesUtil.js
@@ -1,6 +1,6 @@
 import { readPage, constructPage } from "./templatingEngine.js";
 
-const index = readPage("./public/pages/index.html");
+const index = readPage("index.html");
 export const indexPage = constructPage(index, {
   documentTitle: "Overview",
   active: "overview",
