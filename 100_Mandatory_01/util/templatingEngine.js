07af0eb0e6fbdad2cf9a220abb28b08d158592d9
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-17 11:14:22 +0100

    process.join on readPage

--- 100_Mandatory_01/util/templatingEngine.js
+++ 100_Mandatory_01/util/templatingEngine.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export function readPage(filePath) {
-  return fs.readFileSync(path.resolve(filePath)).toString();
+  return fs.readFileSync(path.join(process.cwd(), filePath)).toString();
 }
 
 export function constructPage(page, options = {}) {
