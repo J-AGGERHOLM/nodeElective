3f5917f024ca7478e6f8e23e607a2ec098254249
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-03-19 10:04:01 +0100

    dynamic styling

--- 08_svelte_family/src/components/Child/Child.svelte
+++ 08_svelte_family/src/components/Child/Child.svelte
@@ -2,6 +2,30 @@
     let {name, familySheep, isGirl} = $props();
 </script>
 <div
-class={familySheep}>
+class={familySheep || "not-a-sheep"}
+class:is-girl={isGirl}
+class:is-boy={!isGirl}
+>
 <h3>I'm {name}</h3>
 </div>
+
+<style>
+    div{
+        color: aliceblue;
+    }
+    .black-sheep{
+        background-color: rebeccapurple;
+    }
+    .grey-sheep {
+        background-color: yellowgreen;
+    }
+    .not-a-sheep{
+        background-color: plum;
+    }
+    .is-girl {
+        border: 4px dashed palevioletred;
+    }
+    .is-boy {
+        border: 4px dashed darkblue;
+    }
+</style>
