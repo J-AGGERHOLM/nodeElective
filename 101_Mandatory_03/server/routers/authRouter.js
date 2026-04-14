720290b093291fbec93d63bf3de9e6dd566de016
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-04-14 11:12:52 +0200

    auth router added

---
+++ 101_Mandatory_03/server/routers/authRouter.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/auth/login", (req, res) => {
+  //compare password to DB here:
+  const isMatch = false;
+  if (isMatch) {
+    req.session.loggedIn = true;
+    res.status(200).send({ message: "You're logged in" });
+  } else {
+    //if not a match, return false for toastr alert:
+    req.session.loggedIn = false;
+    res.status(401).send({ message: "Invalid credentials" });
+  }
+});
+
+router.get("/auth/logout", (req, res) => {
+  req.session.destroy((error) => {
+    res.status(200).send({ data: "You're logged out" });
+  });
+});
+
+export default router;
