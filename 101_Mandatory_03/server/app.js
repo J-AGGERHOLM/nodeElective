0f1b7bbde14e91d32a498bb074ce339fc15e3eac
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-04-14 11:27:51 +0200

    app.js set up

---
+++ 101_Mandatory_03/server/app.js
@@ -0,0 +1,60 @@
+import "dotenv/config";
+
+import express from "express";
+const app = express();
+
+import helmet from "helmet";
+app.use(helmet())
+
+import cors from "cors";
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    credentials: true,
+  }),
+);
+
+import session from "express-session";
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  }),
+);
+
+
+import { rateLimit } from "express-rate-limit";
+
+const generalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(generalLimiter);
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+});
+
+app.use("/auth", authLimiter);
+
+
+//======== Routers ========//
+import authRouter from "./routers/authRouter.js";
+app.use(authRouter);
+
+const PORT = process.env.PORT ?? 8080;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
--- 10_Norrebro_Eaterries/server/app.js
+++ 10_Norrebro_Eaterries/server/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 import express from "express";
+import helmet from ""
 
 const app = express();
 
