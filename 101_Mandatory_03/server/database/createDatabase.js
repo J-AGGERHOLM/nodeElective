cf90290b06f6bfd46708235726d3195ff6917522
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-04-17 15:01:17 +0200

    ddl dml and added .db file to gitIgnore

--- 101_Mandatory_03/server/database/createDatabase.js
+++ 101_Mandatory_03/server/database/createDatabase.js
@@ -1,10 +1,10 @@
 import db from "./connection.js";
 
-console.log(process.argv.includes("--delete"))
+console.log(process.argv.includes("--delete"));
 const deleteMode = process.argv.includes("--delete");
 
 if (deleteMode) {
-    await db.exec(`DROP TABLE IF EXISTS users;`)
+  await db.exec(`DROP TABLE IF EXISTS users;`);
 }
 
 /* Conventions for sql 
@@ -20,15 +20,12 @@ await db.exec(`
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username VARCHAR(100) NOT NULL UNIQUE,
         password TEXT NOT NULL,
-        role TEXT CHECK ( role IN ("Admin", "user))
+        role TEXT CHECK ( role IN ("Admin", "User"))
     );
 `);
 
 //DML
 //seeding
 if (deleteMode) {
-    await db.run(`INSERT INTO users (recipe_name) VALUES ('Potato Pancakes');`);
-    await db.run(`INSERT INTO recipes VALUES ('2', 'Baked Potato', "Also known as a jacket potato. It's a treat in the winter months.", 12);`);
-    await db.run(`INSERT INTO ingredients (recipe_id, ingredient_name, units, unit_of_measurement) VALUES (1, 'flour', '0.06', 'kg')`);
-    await db.run(`INSERT INTO ingredients (recipe_id, ingredient_name, units, unit_of_measurement) VALUES (2, 'bacon', '1', 'kg')`);
+  await db.run(`INSERT INTO users (username, password, role) VALUES ('admin01', 'secretPassword', 'Admin');`);
 }
